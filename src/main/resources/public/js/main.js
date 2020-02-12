80cdfdf2a11cc858aa9cfab17e1519438db82a18
Nazarii Tkachuk <nazar.tkachuk.28@gmail.com>
2020-02-12 15:27:25 +0200

    added ui

---
+++ src/main/resources/public/js/main.js
@@ -0,0 +1,11 @@
+var app = angular.module("springApp", []);
+
+app.controller("AppCtrl", function ($scope) {
+    $scope.websites = [{
+        iconImageUrl:' ',
+        id:'myID',
+        website:'pornhub.com',
+        title:'milf',
+        description:'skip it'
+    }];
+});
