2402f76c033106a1b31d1868ad986ebe283fb5a9
Nazarii Tkachuk <nazar.tkachuk.28@gmail.com>
2020-02-13 22:41:05 +0200

    added ability to take websites from https://api.stackexchange.com/

---
+++ src/main/java/com/tkachuk/stackoverflow/service/StackExchangeClient.java
@@ -0,0 +1,33 @@
+package com.tkachuk.stackoverflow.service;
+
+import com.tkachuk.stackoverflow.model.SiteDto;
+import com.tkachuk.stackoverflow.model.SitesDto;
+import org.apache.http.client.HttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.springframework.http.client.ClientHttpRequestFactory;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.List;
+
+@Component
+public class StackExchangeClient {
+    private HttpClient httpClient = HttpClientBuilder.create().build();
+    private ClientHttpRequestFactory requestFactory = new HttpComponentsClientHttpRequestFactory(httpClient);
+    private RestTemplate restTemplate = new RestTemplate(requestFactory);
+
+    public List<SiteDto> getSites() {
+        String url = "https://api.stackexchange.com/2.2/sites?page=1&pagesize=9999&filter=!Fn4IB7S7T4v-QOAVmH9Zka8Ax*";
+
+        try {
+            SitesDto response = restTemplate.getForObject(new URI(url), SitesDto.class);
+            return response.getItems();
+
+        } catch (URISyntaxException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
--- src/main/java/com/tkachuk/stackoverflow/service/StackoverflowService.java
+++ src/main/java/com/tkachuk/stackoverflow/service/StackoverflowService.java
@@ -1,23 +1,54 @@
 package com.tkachuk.stackoverflow.service;
 
+import com.google.common.collect.ImmutableList;
+import com.tkachuk.stackoverflow.model.SiteDto;
 import com.tkachuk.stackoverflow.model.StackoverflowWebsite;
 import com.tkachuk.stackoverflow.repository.StackoverflowWebsiteRepository;
+import lombok.NonNull;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+import static java.util.stream.Collectors.collectingAndThen;
+import static java.util.stream.Collectors.toList;
+
 @Service
 public class StackoverflowService {
-
     private final StackoverflowWebsiteRepository repository;
+    private final StackExchangeClient stackExchangeClient;
 
-    @Autowired
-    public StackoverflowService(StackoverflowWebsiteRepository repository) {
+    public StackoverflowService(StackoverflowWebsiteRepository repository, StackExchangeClient stackExchangeClient) {
         this.repository = repository;
+        this.stackExchangeClient = stackExchangeClient;
     }
 
-    public List<StackoverflowWebsite> findAll() {
-        return repository.findAll();
+    private StackoverflowWebsite toStackoverflowWebsite(@NonNull SiteDto input) {
+        return new StackoverflowWebsite(
+                input.getSite_url().substring("http:// ".length(), input.getSite_url().length() - ".com".length()),
+                input.getSite_url(),
+                input.getFavicon_url(),
+                input.getName(),
+                input.getAudience()
+        );
     }
+
+    /**
+     * Finds and returns all StackoverflowWebsites from the https://api.stackexchange.com/
+     *
+     * @return list of websites from https://api.stackexchange.com/
+     */
+    public List<StackoverflowWebsite> findAll() {
+        return stackExchangeClient.getSites().stream()
+                .map(this::toStackoverflowWebsite)
+                .collect(collectingAndThen(toList(), ImmutableList::copyOf));
+    }
+
+    /**
+     * Finds and returns all StackoverflowWebsites from the DB
+     * @return list of websites from db
+     */
+//    public List<StackoverflowWebsite> findAll() {
+//        return repository.findAll();
+//    }
 }
