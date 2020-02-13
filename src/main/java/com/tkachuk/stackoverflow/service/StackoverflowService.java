06f777dd0670ff8c8ca9ff2fb1174bb18f94bc39
Nazarii Tkachuk <nazar.tkachuk.28@gmail.com>
2020-02-13 12:10:32 +0200

    fixed ui

--- src/main/java/com/tkachuk/stackoverflow/service/StackoverflowService.java
+++ src/main/java/com/tkachuk/stackoverflow/service/StackoverflowService.java
@@ -19,6 +19,34 @@ public class StackoverflowService {
                 "Stack Overflow",
                 "For programmers")
         );
+        items.add(new StackoverflowWebsite(
+                "serverfault",
+                "http://serverfault.com",
+                "http://cdn.sstatic.net/Sites/serverfault/img/favicon.ico",
+                "Server Fault",
+                "For system administrators")
+        );
+        items.add(new StackoverflowWebsite(
+                "superuser",
+                "http://superuser.com",
+                "http://cdn.sstatic.net/Sites/superuser/img/favicon.ico",
+                "Super User",
+                "For computer enthusiasts")
+        );
+        items.add(new StackoverflowWebsite(
+                "askubuntu",
+                "http://askubuntu.com",
+                "http://cdn.sstatic.net/Sites/askubuntu/img/favicon.ico",
+                "Ask Ubuntu",
+                "For Ubuntu users")
+        );
+        items.add(new StackoverflowWebsite(
+                "wordpress",
+                "http://wordpress.com",
+                "http://cdn.sstatic.net/Sites/wordpress/img/favicon.ico",
+                "WordPress",
+                "For WordPress users")
+        );
     }
 
     public List<StackoverflowWebsite> findAll() {
