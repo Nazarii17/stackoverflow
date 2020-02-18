88615654e28dec7f659effb30ecec9ba2fc5c6ab
Nazarii Tkachuk <nazar.tkachuk.28@gmail.com>
2020-02-18 20:56:21 +0200

    fixed HealthService.java

--- src/main/java/com/tkachuk/stackoverflow/service/HealthService.java
+++ src/main/java/com/tkachuk/stackoverflow/service/HealthService.java
@@ -1,7 +1,9 @@
 package com.tkachuk.stackoverflow.service;
 
+import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+@Service
 public class HealthService {
     private RestTemplate restTemplate = new RestTemplate();
 
