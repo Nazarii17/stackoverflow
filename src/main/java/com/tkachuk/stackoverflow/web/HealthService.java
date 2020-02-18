817f7bdf660cb6fff379c891b1248b7302f298c1
Nazarii Tkachuk <nazar.tkachuk.28@gmail.com>
2020-02-18 20:44:16 +0200

    HealthService.getStatus changed to ResponseEntity<String>

--- src/main/java/com/tkachuk/stackoverflow/web/HealthService.java
+++ src/main/java/com/tkachuk/stackoverflow/web/HealthService.java
@@ -1,5 +1,7 @@
 package com.tkachuk.stackoverflow.web;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,7 +10,13 @@ import java.util.Random;
 @RestController
 public class HealthService {
     @RequestMapping("/health")
-    public String getStatus() {
-        return "OK";
+    public ResponseEntity<String> getStatus() {
+        Random randomGenerator = new Random();
+        int randomInt = randomGenerator.nextInt(2);
+        if (randomInt == 1) {
+            return new ResponseEntity<>("ERROR", HttpStatus.INTERNAL_SERVER_ERROR);
+        } else {
+            return new ResponseEntity<>("OK", HttpStatus.OK);
+        }
     }
 }
